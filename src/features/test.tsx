a7dcf2cc8a303374660ddfdaa010dc1e240550b7
avipowar <powaravinash06/2gmail.com>
2026-01-15 11:34:07 +0530

    bun + next.js + shardcn + setup the project

---
+++ src/features/test.tsx
@@ -0,0 +1,11 @@
+import { Button } from "@/components/ui/button";
+
+export const TestComponent = () => {
+  return (
+    <div>
+      <Button>
+        <p className=" text-red-700 hover:bg-white">i am testing</p>
+      </Button>
+    </div>
+  );
+};
